cb35fcf9b05d6ec4485603e3b5f4f65d608582f2
liuhao <lhcs@pku.edu.cn>
2023-12-22 09:04:21 +0000

    move utils out of ftb impl

--- boom_scala/ifu/bpd/ftb.scala
+++ boom_scala/ifu/bpd/ftb.scala
@@ -30,69 +30,6 @@ trait FTBParams extends HasBoomFTBParameters {
   def JMP_OFFSET_LEN = 20
 }  
 
-object SignExt {
-  def apply(a: UInt, len: Int): UInt = {
-    val aLen = a.getWidth
-    val signBit = a(aLen-1)
-    if (aLen >= len) a(len-1,0) else Cat(Fill(len - aLen, signBit), a)
-  }
-}
-
-object ZeroExt {
-  def apply(a: UInt, len: Int): UInt = {
-    val aLen = a.getWidth
-    if (aLen >= len) a(len-1,0) else Cat(0.U((len - aLen).W), a)
-  }
-}
-
-trait BPUUtils extends HasBoomFTBParameters {
-  // circular shifting
-  def circularShiftLeft(source: UInt, len: Int, shamt: UInt): UInt = {
-    val res = Wire(UInt(len.W))
-    val higher = source << shamt
-    val lower = source >> (len.U - shamt)
-    res := higher | lower
-    res
-  }
-
-  def circularShiftRight(source: UInt, len: Int, shamt: UInt): UInt = {
-    val res = Wire(UInt(len.W))
-    val higher = source << (len.U - shamt)
-    val lower = source >> shamt
-    res := higher | lower
-    res
-  }
-
-  // To be verified
-  def satUpdate(old: UInt, len: Int, taken: Bool): UInt = {
-    val oldSatTaken = old === ((1 << len)-1).U
-    val oldSatNotTaken = old === 0.U
-    Mux(oldSatTaken && taken, ((1 << len)-1).U,
-      Mux(oldSatNotTaken && !taken, 0.U,
-        Mux(taken, old + 1.U, old - 1.U)))
-  }
-
-  def signedSatUpdate(old: SInt, len: Int, taken: Bool): SInt = {
-    val oldSatTaken = old === ((1 << (len-1))-1).S
-    val oldSatNotTaken = old === (-(1 << (len-1))).S
-    Mux(oldSatTaken && taken, ((1 << (len-1))-1).S,
-      Mux(oldSatNotTaken && !taken, (-(1 << (len-1))).S,
-        Mux(taken, old + 1.S, old - 1.S)))
-  }
-
-  def getFallThroughAddr(start: UInt, carry: Bool, pft: UInt) = {
-    val higher = start.head(vaddrBitsExtended-log2Ceil(predictWidth)-instOffsetBits)
-    Cat(Mux(carry, higher+1.U, higher), pft, 0.U(instOffsetBits.W))
-  }
-
-  // def foldTag(tag: UInt, l: Int): UInt = {
-  //   val nChunks = (tag.getWidth + l - 1) / l
-  //   val chunks = (0 until nChunks).map { i =>
-  //     tag(min((i+1)*l, tag.getWidth)-1, i*l)
-  //   }
-  //   ParallelXOR(chunks)
-  // }
-}
 
 class FtbSlot(val offsetLen: Int, val subOffsetLen: Option[Int] = None)(implicit p: Parameters) extends BoomBundle with FTBParams {
   if (subOffsetLen.isDefined) {
---
+++ boom_scala/ifu/bpd/utils.scala
@@ -0,0 +1,78 @@
+package boom.ifu
+
+import chisel3._
+import chisel3.util._
+
+import freechips.rocketchip.config.{Field, Parameters}
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.tilelink._
+import freechips.rocketchip.util._
+
+import boom.common._
+import boom.util._
+
+import scala.math.min
+
+object SignExt {
+  def apply(a: UInt, len: Int): UInt = {
+    val aLen = a.getWidth
+    val signBit = a(aLen-1)
+    if (aLen >= len) a(len-1,0) else Cat(Fill(len - aLen, signBit), a)
+  }
+}
+
+object ZeroExt {
+  def apply(a: UInt, len: Int): UInt = {
+    val aLen = a.getWidth
+    if (aLen >= len) a(len-1,0) else Cat(0.U((len - aLen).W), a)
+  }
+}
+
+trait BPUUtils extends HasBoomFTBParameters {
+  // circular shifting
+  def circularShiftLeft(source: UInt, len: Int, shamt: UInt): UInt = {
+    val res = Wire(UInt(len.W))
+    val higher = source << shamt
+    val lower = source >> (len.U - shamt)
+    res := higher | lower
+    res
+  }
+
+  def circularShiftRight(source: UInt, len: Int, shamt: UInt): UInt = {
+    val res = Wire(UInt(len.W))
+    val higher = source << (len.U - shamt)
+    val lower = source >> shamt
+    res := higher | lower
+    res
+  }
+
+  // To be verified
+  def satUpdate(old: UInt, len: Int, taken: Bool): UInt = {
+    val oldSatTaken = old === ((1 << len)-1).U
+    val oldSatNotTaken = old === 0.U
+    Mux(oldSatTaken && taken, ((1 << len)-1).U,
+      Mux(oldSatNotTaken && !taken, 0.U,
+        Mux(taken, old + 1.U, old - 1.U)))
+  }
+
+  def signedSatUpdate(old: SInt, len: Int, taken: Bool): SInt = {
+    val oldSatTaken = old === ((1 << (len-1))-1).S
+    val oldSatNotTaken = old === (-(1 << (len-1))).S
+    Mux(oldSatTaken && taken, ((1 << (len-1))-1).S,
+      Mux(oldSatNotTaken && !taken, (-(1 << (len-1))).S,
+        Mux(taken, old + 1.S, old - 1.S)))
+  }
+
+  def getFallThroughAddr(start: UInt, carry: Bool, pft: UInt) = {
+    val higher = start.head(vaddrBitsExtended-log2Ceil(predictWidth)-instOffsetBits)
+    Cat(Mux(carry, higher+1.U, higher), pft, 0.U(instOffsetBits.W))
+  }
+
+  // def foldTag(tag: UInt, l: Int): UInt = {
+  //   val nChunks = (tag.getWidth + l - 1) / l
+  //   val chunks = (0 until nChunks).map { i =>
+  //     tag(min((i+1)*l, tag.getWidth)-1, i*l)
+  //   }
+  //   ParallelXOR(chunks)
+  // }
+}
