12d335badfe5604a367716eb51462a41f3d9ab3d
liuhao <lhcs@pku.edu.cn>
2024-02-23 08:28:00 +0000

    Removed Original Branch Predictor

--- boom_scala/common/config-mixins.scala
+++ boom_scala/common/config-mixins.scala
@@ -458,23 +458,6 @@ class WithBlockBPD extends Config((site, here, up) => {
       globalHistoryLength = 64,
       localHistoryLength = 1,
       localHistoryNSets = 0,
-      branchPredictor = ((resp_in: BranchPredictionBankResponse, p: Parameters) => {
-        val loop = Module(new LoopBranchPredictorBank()(p))
-        val tage = Module(new TageBranchPredictorBank()(p))
-        val btb = Module(new BTBBranchPredictorBank()(p))
-        val bim = Module(new BIMBranchPredictorBank()(p))
-        val ubtb = Module(new FAMicroBTBBranchPredictorBank()(p))
-        val preds = Seq(loop, tage, btb, ubtb, bim)
-        preds.map(_.io := DontCare)
-
-        ubtb.io.resp_in(0)  := resp_in
-        bim.io.resp_in(0)   := ubtb.io.resp
-        btb.io.resp_in(0)   := bim.io.resp
-        tage.io.resp_in(0)  := btb.io.resp
-        loop.io.resp_in(0)  := tage.io.resp
-
-        (preds, loop.io.resp)
-      }),
       newBranchPredictor = ((resp_in: BPBankResponse, p: Parameters) => {
         val fauftb = Module(new FauFTB()(p))
         val ftb = Module(new FTB()(p))
