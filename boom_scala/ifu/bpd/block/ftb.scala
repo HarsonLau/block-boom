1926cf3e34c974c5de99051e6d4a10aaf692d8e7
liuhao <lhcs@pku.edu.cn>
2024-02-28 08:46:34 +0000

    FTB set count 512 -> 256
    In this commit, the set count of the FTB component has been adjusted from 512 to 256. This change is made to align the total number of branch slots with that in the original BOOM BTB. The adjustment facilitates a more direct comparison of hardware overhead and performance between the FTB component and the original BOOM BTB.

--- boom_scala/ifu/bpd/block/ftb.scala
+++ boom_scala/ifu/bpd/block/ftb.scala
@@ -14,9 +14,9 @@ import boom.util._
 import scala.math.min
 
 trait FTBParams extends HasBoomFTBParameters {
-  val numEntries = 128 * 2 * 4 / 2
+  val numEntries = 128 * 4 / 2
   val numWays    = 2
-  val numSets    = numEntries/numWays // 512
+  val numSets    = numEntries/numWays // 256
   val tagSize    = 20
 
 
