b17ac8be927a3ade39245ef64bdec6937c3b7291
liuhao <lhcs@pku.edu.cn>
2024-03-30 08:19:13 +0000

    modify max period

--- spec_run/gen_ctrl.sh
+++ spec_run/gen_ctrl.sh
@@ -2,6 +2,13 @@
 # take in two arguments , the first specifying the benchmark name ,the second specifying the config hash
 benchname=$1
 config=$2
+maxperiod=500
+
+# if there is a third argument ,set maxperiod
+if [ $# -eq 3 ];then
+    maxperiod=$3
+fi
+
 benchlist=('400.perlbench' '401.bzip2' '403.gcc' '429.mcf'  '445.gobmk' '458.sjeng'   '462.libquantum'   '464.h264ref'   '471.omnetpp'   '473.astar'   '483.xalancbmk'   '999.specrand')
 for bench in ${benchlist[*]}
 do
@@ -20,5 +27,5 @@ logname=$(realpath ./data/$benchname-$config-$logcnt.log)
 echo "eventsel: 0" > $ctrlfile
 echo "maxevent: 200000000">> $ctrlfile
 echo "warmupinst: 0">>$ctrlfile
-echo "maxperiod: 2000">>$ctrlfile
+echo "maxperiod: $maxperiod">>$ctrlfile
 echo "logname: $logname">>$ctrlfile
